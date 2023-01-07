0be42abf05c10a0d5b93cb524ed90d0a19aa1980
Aaron Hamilton <aacnchamilton@gmail.com>
2023-01-07 15:30:32 -0700

    finished code video and word doc

--- BE-Promineo-Tech/Week-07-Introduction_to_MySQL/src/main/java/projects/exception/DbException.java
+++ BE-Promineo-Tech/Week-07-Introduction_to_MySQL/src/main/java/projects/exception/DbException.java
@@ -2,9 +2,6 @@ package projects.exception;
 
 public class DbException extends RuntimeException {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	
 	public DbException(String message) {
