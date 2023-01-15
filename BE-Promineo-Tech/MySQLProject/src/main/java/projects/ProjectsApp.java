396f051833a0980e82b1fb62bb99b1da1a385491
Aaron Hamilton <aacnchamilton@gmail.com>
2023-01-14 21:33:17 -0700

    done thru page 7 of week 9 Coding assignment

--- BE-Promineo-Tech/MySQLProject/src/main/java/projects/ProjectsApp.java
+++ BE-Promineo-Tech/MySQLProject/src/main/java/projects/ProjectsApp.java
@@ -1,11 +1,68 @@
 package projects;
 
-import projects.dao.DbConnection;
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
+
+import projects.exception.DbException;
 
 public class ProjectsApp {
 
+	// @formatter:off
+	List<String> operations = List.of(
+		"1). Add a project." 
+	);
+	// @formatter:On
+	
+	Scanner scanner = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		DbConnection.getConnection();
+		
+		new ProjectsApp().processUserSelections();
+	}
+
+	private void processUserSelections() {
+		boolean done = false;
+		while (!done) {
+			try {
+				int selection = getUserSelection();
+			}
+			catch(Exception e) {
+				System.out.println("\nError: " + e + "Try again.");
+			}
+		}
+	}
+
+	private int getUserSelection() {
+		printOperations();
+		Integer input = getIntInput("Enter a manu selection");
+		return Objects.isNull(input) ? -1 : input;
+	}
+
+	private Integer getIntInput(String prompt) {
+		String input = getStringInput(prompt);
+		if (Objects.isNull(input)) {
+			return null;
+		}
+		try {
+			return Integer.valueOf(input);
+		}
+		catch (NumberFormatException e) {
+			throw new DbException(input + " is not a valid number.");
+		}
+		
+		
+	}
+
+	private String getStringInput(String prompt) {
+		System.out.print(prompt + ": ");
+		String input = scanner.nextLine();
+		return Objects.isNull(input) ? null : input.trim();
+	}
+
+	private void printOperations() {
+		System.out.println("\nThese are the available selections. Press the Enter key to quit:");
+		operations.forEach(line -> System.out.println("  " + line));
 	}
 
 }
