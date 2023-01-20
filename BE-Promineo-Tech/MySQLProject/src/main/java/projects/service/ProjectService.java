c23c7b415358e67b25296cfc6681384410bb56b0
aacnchamilton@gmail.com <aacnchamilton@gmail.com>
2023-01-20 16:52:14 -0700

    finished with Java code and tested it

--- BE-Promineo-Tech/MySQLProject/src/main/java/projects/service/ProjectService.java
+++ BE-Promineo-Tech/MySQLProject/src/main/java/projects/service/ProjectService.java
@@ -1,12 +1,15 @@
 package projects.service;
 
+import projects.dao.ProjectDao;
 import projects.entity.Project;
 
 public class ProjectService {
+	
+	private ProjectDao projectDao = new ProjectDao();
 
 	public Project addProject(Project project) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return projectDao.insertProject(project);
 	}
 
 }
