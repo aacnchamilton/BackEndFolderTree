3fa405c7b40a3ebe54d4169897d74b8156e3a521
aacnchamilton@gmail.com <aacnchamilton@gmail.com>
2023-01-20 16:03:51 -0700

    finished with createProject() method

--- BE-Promineo-Tech/MySQLProject/src/main/java/projects/ProjectsApp.java
+++ BE-Promineo-Tech/MySQLProject/src/main/java/projects/ProjectsApp.java
@@ -1,10 +1,13 @@
 package projects;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
 
+import projects.entity.Project;
 import projects.exception.DbException;
+import projects.service.ProjectService;
 
 public class ProjectsApp {
 
@@ -15,6 +18,7 @@ public class ProjectsApp {
 	// @formatter:On
 	
 	Scanner scanner = new Scanner(System.in);
+	private ProjectService projectService = new ProjectService();
 	
 	public static void main(String[] args) {
 		
@@ -30,6 +34,9 @@ public class ProjectsApp {
 					case -1:
 						done = exitMenu();
 						break;
+					case 1:
+						createProject();
+						break;
 					default:
 						System.out.println("\n" + selection + " is not a valid selection. Try again.");
 						break;
@@ -39,6 +46,35 @@ public class ProjectsApp {
 			catch(Exception e) {
 				System.out.println("\nError: " + e + "Try again.");
 			}
+		}
+	}
+
+	private void createProject() {
+		String projectName = getStringInput("Enter the project name:");
+		BigDecimal estimatedHours = getDecimalInput("Enter the estimated hours:");
+		BigDecimal actualHours = getDecimalInput("Enter the actual hours:");
+		Integer difficulty = getIntInput("Enter the project difficulty (1-5):");
+		String notes = getStringInput("Enter the project notes:");
+		Project project = new Project();
+		project.setProjectName(projectName);
+		project.setEstimatedHours(estimatedHours);
+		project.setActualHours(actualHours);
+		project.setDifficulty(difficulty);
+		project.setNotes(notes);
+		Project dbProject = projectService.addProject(project);
+		System.out.println("You have successfully created project: " + dbProject);
+	}
+
+	private BigDecimal getDecimalInput(String prompt) {
+		String input = getStringInput(prompt);
+		if (Objects.isNull(input)) {
+			return null;
+		}
+		try {
+			return new BigDecimal(input).setScale(2);
+		}
+		catch (NumberFormatException e) {
+			throw new DbException(input + " is not a valid decimal number.");
 		}
 	}
 
---
+++ BE-Promineo-Tech/MySQLProject/src/main/java/projects/service/ProjectService.java
@@ -0,0 +1,12 @@
+package projects.service;
+
+import projects.entity.Project;
+
+public class ProjectService {
+
+	public Project addProject(Project project) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
