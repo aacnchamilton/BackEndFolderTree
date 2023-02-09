71c00816a6fb5aed37ed68332218d676f9112e62
aacnchamilton@gmail.com <aacnchamilton@gmail.com>
2023-02-09 08:02:08 -0700

    copied over from STS workspace, still need to setup against this directory

---
+++ BE-Promineo-Tech/SpringbootJeepSales/jeep-sales/src/main/java/com/promineotech/jeep/JeepSales.java
@@ -0,0 +1,17 @@
+package com.promineotech.jeep;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class JeepSales {
+  
+  
+  
+  public static void main(String[] args) {
+   
+    SpringApplication.run(JeepSales.class, args);
+    
+  }
+
+}
